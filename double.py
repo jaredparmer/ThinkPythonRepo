c930000f948263eab789cb5206422b2927dd8dcc
jaredparmer <jaredparmer@gmail.com>
2020-08-22 14:58:46 +0200

    finished exercise 16.2

--- double.py
+++ double.py
@@ -26,6 +26,15 @@ def birthday_info(birthdate):
           f"{diff.seconds % 60} seconds!")
 
 
+def double_day(bday1, bday2, factor=2):
+    gap = bday2 - bday1
+    double_day = bday2 + gap / (factor - 1)
+    print(f"Your {factor}-Factor Day is ", end='')
+    print(double_day.strftime('%B') + ' ' + double_day.strftime('%d') + ', '
+          + double_day.strftime('%Y'))
+
+
+# driver code for birthday_info
 while True:
     bday = input("What is your birthday? (yyyy-mm-dd, 'Q' to quit) ")
     if bday == 'Q':
@@ -35,3 +44,27 @@ while True:
         data[i] = int(data[i])
     birthday = datetime(*data)
     birthday_info(birthday)
+
+# driver code for double_day
+while True: 
+    bday1 = input("What is the first birthday? (yyyy-mm-dd, 'Q' to quit) ")
+    if bday1 == 'Q':
+        break
+    data = bday1.split(sep='-')
+    for i in range(len(data)):
+        data[i] = int(data[i])
+    bday1 = datetime(*data)
+
+    bday2 = input("What is the second birthday? (yyyy-mm-dd, 'Q' to quit) ")
+    if bday2 == 'Q':
+        break
+    data = bday2.split(sep='-')
+    for i in range(len(data)):
+        data[i] = int(data[i])
+    bday2 = datetime(*data)
+
+    factor = input("What is the factor? (integer, 'Q' to quit) ")
+    if factor == 'Q':
+        break
+
+    double_day(bday1, bday2, int(factor))
