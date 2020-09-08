156e3a009532aab5706f8dba70bdbfe1ccfdcdff
jaredparmer <jaredparmer@gmail.com>
2020-09-08 17:39:48 +0200

    added checks for straights and four of a kind to PokerHand.py

--- PokerHand.py
+++ PokerHand.py
@@ -16,60 +16,90 @@ class PokerHand(Hand):
         self.suits = {}
         self.ranks = {}
 
+
+    def check_sets(self, *t):
+        """ t is a list of ints, with number of elements being the number of
+        sets to check for, and the elements being the length within each set to
+        check for. t should be sorted from longest set to shortest set.
+        For example, checking for two pairs is t = [2, 2], checking for a full
+        house is t = [3, 2], and checking for four of a kind is t = [4]. """
+        for need, have in zip(t, self.sets):
+            if need > have:
+                return False
+        return True
+
+        
+    def classify(self):
+        """ identifies best hand (pair, two pair, etc.) in poker hand. """
+
+        """ set up: build histogram of ranks, histogram of suits, and sorted
+        list of rank sets in hand. """
+        self.ranks = {}
+        for card in self.cards:
+            self.ranks[card.rank] = self.ranks.get(card.rank, 0) + 1
+            
+        self.suits = {}
+        for card in self.cards:
+            self.suits[card.suit] = self.suits.get(card.suit, 0) + 1
+
+        self.sets = list(self.ranks.values())
+        self.sets.sort(reverse=True)
+
+        """ identification phase. """
+        # TBD
+
+
     def has_flush(self):
-        self.suit_hist()
         for val in self.suits.values():
             if val >= 5:
                 return True
         return False
 
+
     def has_four(self):
         """ checks for four of a kind. """
-        self.rank_hist()
-        for val in self.ranks.values():
-            if val >= 4:
-                return True
-        return False
+        return self.check_sets(4)
+
+
+    def has_full_house(self):
+        return self.check_sets(3, 2)
+
 
     def has_pair(self):
-        self.rank_hist()
-        for val in self.ranks.values():
-            if val >= 2:
-                return True
+        return self.check_sets(2)
+
+
+    def has_straight(self):
+        ranks = list(self.ranks.keys())
+        ranks.sort()
+        prev_rank = ranks[0] - 1
+        # length of run found so far; resets to 1 when run is broken
+        count = 0
+        for i in range(len(ranks)):
+            if ranks[i] - prev_rank == 1:
+                count += 1
+                if count == n:
+                    return True
+            else:
+                count = 1
+
+            prev_rank = ranks[i]
+
+        """ special case: 10-J-Q-K-A type runs, where Aces, coded as rank 1,
+        also should be considered to have highest rank. """
+        if count == n - 1 and 1 in ranks:
+            return True
+        
         return False
 
+        
     def has_three(self):
         """ checks for three of a kind. """
-        self.rank_hist()
-        for val in self.ranks.values():
-            if val >= 3:
-                return True
-        return False
+        return self.check_sets(3)
+
 
     def has_two_pair(self):
-        self.rank_hist()
-        vals = list(self.ranks.values())
-        vals.sort(reverse=True)
-        if vals[0] >= 2 and vals[1] >= 2:
-            return True
-        return False
-        
-
-    def rank_hist(self):
-        """ builds a histogram of ranks (card values, Ace to King) that appear
-        in the hand, stores in attribute ranks.
-        """
-        self.ranks = {}
-        for card in self.cards:
-            self.ranks[card.rank] = self.ranks.get(card.rank, 0) + 1
-
-    def suit_hist(self):
-        """ builds a histogram of suits that appear in the hand, stores in
-        attribute suits.
-        """
-        self.suits = {}
-        for card in self.cards:
-            self.suits[card.suit] = self.suits.get(card.suit, 0) + 1
+        return self.check_sets(2, 2)
 
 
 def main():
@@ -77,10 +107,23 @@ def main():
     deck.shuffle()
     for i in range(7):
         hand = PokerHand(f"hand {i + 1}")
-        deck.draw(hand, 7)
-        hand.sort()
+        deck.draw(hand, 7)   
+        hand.classify()
         print(hand)
-        print("has three of a kind: " + str(hand.has_three()))
+        if hand.has_pair():
+            print("has two of a kind")
+        if hand.has_two_pair():
+            print("has two pair")
+        if hand.has_three():
+            print("has three of a kind")
+        if hand.has_straight():
+            print("has straight")
+        if hand.has_flush():
+            print("has flush")
+        if hand.has_full_house():
+            print("has full house")
+        if hand.has_four():
+            print("has four of a kind")
         print('')
 
 
